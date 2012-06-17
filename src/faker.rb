b544aec78ee5eac506bf65300d35aa55ebbfb4f5
namu <namutaka+g@gmail.com>
2012-06-17 21:58:32 +0900

    単語の定義方法を変更

--- src/faker.rb
+++ src/faker.rb
@@ -5,46 +5,53 @@ require 'pp'
 require 'rubygems'
 require 'faker'
 
-module EnumerateEx
+class Array
   def pick
     self[rand(size)]
   end
 end
 
-class Array
-  include EnumerateEx
-end
-
 
 module Faker
 
+  def self.defwords(name, words)
+    const_set(name.upcase, words.to_a)
+    class_eval <<-EOS
+      class << self
+        define_method("#{name.downcase}") do |count|
+          chars(const_get("#{name}"), count)
+        end
+      end
+    EOS
+  end
+
   # U+3040-309F	Hiragana	平仮名
-  HIRAGANA = ("\u3041".."\u3096").to_a
+  defwords :HIRAGANA, "\u3041".."\u3096"
   # U+30A0-30FF	Katakana	片仮名
-  KATAKANA = ("\u30A1".."\u30F6").to_a
+  defwords :KATAKANA, "\u30A1".."\u30F6"
 
   # U+4E00-9FFF	CJK Unified Ideographs	CJK統合漢字
-  KANJI = ("\u4E00".."\u9FA1").to_a
+  defwords :KANJI, "\u4E00".."\u9FA1"
 
   # U+3000-303F	CJK Symbols and Punctuation	CJKの記号及び句読点
   KUHAKU = "\u3000"
   TOUTEN = "\u3001"
   KUTEN = "\u3002"
-  KIGOU = ("\u3003".."\u301C" ).to_a
+  defwords :KIGOU, "\u3003".."\u301C"
 
   # U+FF00-FFEF	Halfwidth and Fullwidth Forms	半角・全角形
-  FULLWIDTH = ("\uFF01".."\uFF60").to_a
+  defwords :FULLWIDTH, "\uFF01".."\uFF60"
 
   # 文字全般
-  FULLCHARS = [KATAKANA, HIRAGANA,
+  defwords :FULLCHARS, [KATAKANA, HIRAGANA,
     KANJI, KIGOU, FULLWIDTH].flatten
 
-  def self.fullchars(base, count = 5)
+  def self.chars(base, count = 5)
     repeat(count) { base.pick }.join
   end
 
   def self.words(base, num = 5)
-    repeat(num) { fullchars(base, 1 + rand(10)) }
+    repeat(num) { chars(base, 1 + rand(10)) }
   end
 
   def self.digits(digit)
@@ -62,25 +69,12 @@ module Faker
   end
 
   def self.main
-    pp fullchars(FULLWIDTH)
     pp words(FULLCHARS, 5)
     pp digits(10)
+    pp hiragana(10)
   end
 
 end
-
-
-
-=begin
-puts Faker::Lorem.characters(10)
-puts Faker::Lorem.paragraph(2, false)
-puts Faker::Lorem.words(2, false).join("^")
-puts :aaa
-puts [ 0x3000 ].pack('U')
-=end
-
-#pp (10..20).inject([]) {|ret| ret << 1}.join
-#pp [111,22].pick
 
 Faker.main
 
